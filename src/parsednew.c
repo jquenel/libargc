c36ce56b19615fae8857d807b8263d3f67b79129
jquenel <jquenel@student.42.fr>
2019-04-30 00:48:37 +0200

    fix : now ok to have 0 args

--- src/parsednew.c
+++ src/parsednew.c
@@ -5,7 +5,7 @@ t_parsed		*parsednew(t_arg *args, t_argparser *parser)
 {
 	t_parsed	*parsed;
 
-	if (!args || !parser)
+	if (!parser)
 		return (NULL);
 	if (!(parsed = (t_parsed *)malloc(sizeof(t_parsed))))
 		return (NULL);
